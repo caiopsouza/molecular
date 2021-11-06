143bfb148552ba1942053a51bde23be1f26cd443
caiopsouza <caiophilipevs@gmail.com>
2021-11-06 09:14:29 -0300

    Fixed error calculation and some more testings

--- benches/nmr.rs
+++ benches/nmr.rs
@@ -1,30 +1,32 @@
 use criterion::{black_box, criterion_group, criterion_main, Criterion};
 
 fn criterion_benchmark(c: &mut Criterion) {
-    c.bench_function("test", |b| b.iter(|| {
-        let problem = molecular::load_problem("test.nmr");
-        molecular::solve(black_box(&problem));
+    c.bench_function("test.nmr", |b| b.iter(|| {
+        molecular::load_solve_and_format(black_box("test.nmr"));
     }));
 
     c.bench_function("10-3.nmr", |b| b.iter(|| {
-        let problem = molecular::load_problem("10-3.nmr");
-        molecular::solve(black_box(&problem));
+        molecular::load_solve_and_format(black_box("10-3.nmr"));
     }));
 
     c.bench_function("1ppt.nmr", |b| b.iter(|| {
-        molecular::run(molecular::solve_1ppt);
+        molecular::load_solve_and_format(black_box("1ppt.nmr"));
     }));
 
     c.bench_function("2erl.nmr", |b| b.iter(|| {
-        molecular::run(molecular::solve_2erl);
+        molecular::load_solve_and_format(black_box("2erl.nmr"));
     }));
 
     c.bench_function("1fs3.nmr", |b| b.iter(|| {
-        molecular::run(molecular::solve_1fs3);
+        molecular::load_solve_and_format(black_box("1fs3.nmr"));
     }));
 
     c.bench_function("2e7z.nmr", |b| b.iter(|| {
-        molecular::run(molecular::solve_2e7z);
+        molecular::load_solve_and_format(black_box("2e7z.nmr"));
+    }));
+
+    c.bench_function("7nyz.nmr", |b| b.iter(|| {
+        molecular::load_solve_and_format(black_box("7nyz.nmr"));
     }));
 }
 
